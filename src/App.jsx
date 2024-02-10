40bfa5042db4a4f466443e96bf14cedc1afe2441
Aman Kumar <akjha1469@gmail.com>
2024-02-10 20:22:18 +0530

    num

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [number, setNumber] = useState();
+  const [showOTPForm, setShowOTPForm] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("OTP sent on", number);
+    setShowOTPForm(true);
+  };
+
+  const handlePhoneSUbmit = (e) => {
+    setNumber(e.target.value);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="">
+        {!showOTPForm ? (
+          <>
+            <h1>Enter your number</h1>
+            <form action="" onSubmit={handleSubmit}>
+              <input
+                type="text"
+                placeholder="Enter Phone Number"
+                value={number}
+                onChange={handlePhoneSUbmit}
+              />
+              <button type="submit">Send OTP</button>
+            </form>
+          </>
+        ) : (
+          <div>
+            <h1>Enter OTP</h1>
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
